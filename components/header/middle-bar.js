79ced1d45982637c0bd94f781af76f8daeb84e7f
hy.serure <hy.serure@newelevation.com>
2021-08-19 23:35:20 +0300

    Header mobile

--- components/header/middle-bar.js
+++ components/header/middle-bar.js
@@ -15,11 +15,13 @@ import Icon from "@components/icon";
 import Link from "next/link";
 import Templates from "@components/templates/templates";
 import NavList from "@components/templates/nav-list";
-import {bodyOverlay} from "../../lib/helpers";
+import {bodyOverlay, useCurrentWidth} from "../../lib/helpers";
 import SearchField from "@components/header/search/search-field";
 import InstantSearch from "@components/header/search/instant-search";
 
 export default function MiddleBar({data, customer}) {
+    let width = useCurrentWidth();
+
     const [isHovering, setIsHovered] = useState(false);
     const onMouseEnter = () => {
         bodyOverlay().then(r => {
@@ -55,80 +57,91 @@ export default function MiddleBar({data, customer}) {
         <div className="text-sm py-5 bg-white">
             <Container>
                 <div className="flex items-center">
-                    <div className="address-item relative pb-5 -mb-5"
+                    <div className="address-item relative md:pb-5 md:-mb-5"
                          onMouseEnter={onMouseEnter}
                          onMouseLeave={onMouseLeave}>
                         <a href="#" className="block relative pl-5">
                             <Icon icon={faMapMarkerAlt}
                                   className={"absolute left-0 pr-2.5 transform " + (data.address ? "translate-y-2/4" : "top-1/2 -translate-y-2/4")}/>
-                            {data.title}
+
+                            <span className="hidden md:inline">
+
+                                {data.title}
+                                {data.address ? (
+                                    isHovering ? (
+                                        <Icon icon={faChevronUp} className="pl-2.5"/>
+                                    ) : (
+                                        <Icon icon={faChevronDown} className="pl-2.5"/>
+                                    )
+                                ) : (
+                                    ""
+                                )}
+                                {data.timeOpening ? (
+                                    <time className="time block flex items-center"><span
+                                        className="w-2 h-2 rounded-xl inline-block bg-green box-content text-xs  mr-2.5"/>{data.timeOpening}
+                                    </time>
+                                ) : (
+                                    ""
+                                )}
+                            </span>
+                        </a>
+                        <div className="hidden md:block">
                             {data.address ? (
                                 isHovering ? (
-                                    <Icon icon={faChevronUp} className="pl-2.5"/>
+                                    <div
+                                        className="overlay-fade text-xs absolute border border-gray_border border-solid bg-white w-full min-w-min352 p-5 top-full">
+                                        <h3 className="text-xl text-gray_4 font-bold  pb-2.5">{data.title}</h3>
+                                        {
+                                            data.address ? (
+                                                <p className="pb-2.5" dangerouslySetInnerHTML={{__html: data.address}}/>
+                                            ) : (
+                                                ""
+                                            )
+                                        }
+
+                                        {
+                                            data.phone ? (
+                                                <p className="phone pb-2.5">{data.phone}</p>
+
+                                            ) : (
+                                                ""
+                                            )
+                                        }
+
+                                        {
+                                            data.time ? (
+                                                <time className="pb-2.5" dangerouslySetInnerHTML={{__html: data.time}}/>
+                                            ) : (
+                                                ""
+                                            )
+                                        }
+                                        <button className="btn">Change Your Store</button>
+                                    </div>
                                 ) : (
-                                    <Icon icon={faChevronDown} className="pl-2.5"/>
+                                    ""
                                 )
                             ) : (
                                 ""
                             )}
-
-                            {data.timeOpening ? (
-                                <time className="time block flex items-center"><span
-                                    className="w-2 h-2 rounded-xl inline-block bg-green box-content text-xs  mr-2.5"/>{data.timeOpening}
-                                </time>
-                            ) : (
-                                ""
-                            )}
-
-                        </a>
-                        {data.address ? (
-                            isHovering ? (
-                                <div
-                                    className="overlay-fade text-xs absolute border border-gray_border border-solid bg-white w-full min-w-min352 p-5 top-full">
-                                    <h3 className="text-xl text-gray_4 font-bold  pb-2.5">{data.title}</h3>
-                                    {
-                                        data.address ? (
-                                            <p className="pb-2.5" dangerouslySetInnerHTML={{__html: data.address}}/>
-                                        ) : (
-                                            ""
-                                        )
-                                    }
-
-                                    {
-                                        data.phone ? (
-                                            <p className="phone pb-2.5">{data.phone}</p>
-
-                                        ) : (
-                                            ""
-                                        )
-                                    }
-
-                                    {
-                                        data.time ? (
-                                            <time className="pb-2.5" dangerouslySetInnerHTML={{__html: data.time}}/>
-                                        ) : (
-                                            ""
-                                        )
-                                    }
-                                    <button className="btn">Change Your Store</button>
-                                </div>
-                            ) : (
-                                ""
-                            )
-                        ) : (
-                            ""
-                        )}
+                        </div>
 
 
                     </div>
                     <div className="logo absolute left-1/2 transform -translate-x-2/4 max-w-logo-width">
                         <Link href="/">
-                            <a><img src="assets/images/logo.png"/></a>
+                            <a><img src="assets/images/logo.png" className="w-5/6 max-w-logo-width"/></a>
                         </Link>
                     </div>
-                    <ul className="middle-bar-menu flex ml-auto relative py-30px -my-5">
-                        <li className="py-30px -my-30px"><SearchField instantSearchState={instantSearchState} setInstantSearchState={setInstantSearchState}/></li>
-                        <li className="py-30px -my-30px"
+                    <ul className="middle-bar-menu flex ml-auto relative md:py-30px md:-my-5">
+                        {
+                            width > 768 ? (
+                                <li className="py-30px -my-30px"><SearchField instantSearchState={instantSearchState}
+                                                                              setInstantSearchState={setInstantSearchState}/>
+                                </li>
+                            ) : ("")
+                        }
+
+                        <li className="md:py-30px md:-my-30px"
                             onMouseEnter={onCustomerMouseEnter}
                             onMouseLeave={onCustomerMouseLeave}
                         >
@@ -137,28 +150,35 @@ export default function MiddleBar({data, customer}) {
                                 customer.nav ? (
                                     <>
                                         <a href="#"
-                                        ><Icon icon={faUserCircle} className="pr-1.5"/>{customer.title}
-                                            {
-                                                isCustomerHovering ? (
-                                                    <Icon icon={faChevronUp} className="pl-2.5"/>
-                                                ) : (
-                                                    <Icon icon={faChevronDown} className="pl-2.5"/>
-                                                )
-                                            }
+                                        ><Icon icon={faUserCircle} className="pr-1.5"/>
+                                            <span className="hidden md:inline">
+                                                {customer.title}
+                                                {
+                                                    isCustomerHovering ? (
+                                                        <Icon icon={faChevronUp} className="pl-2.5"/>
+                                                    ) : (
+                                                        <Icon icon={faChevronDown} className="pl-2.5"/>
+                                                    )
+                                                }
+                                            </span>
+
                                         </a>
-                                        {isCustomerHovering ? (
+                                        <div className="hidden md:inline">
+                                            {isCustomerHovering ? (
 
-                                            <div
-                                                className="overlay-fade absolute left-0 border border-gray_border border-solid bg-white min-w-min352 p-5 top-full">
-                                                <NavList data={customer.nav} className={"customer-header-menu left-0"}/>
+                                                <div
+                                                    className="overlay-fade absolute left-0 border border-gray_border border-solid bg-white min-w-min352 p-5 top-full">
+                                                    <NavList data={customer.nav} className={"customer-header-menu left-0"}/>
 
-                                                {customer.templates ? (
-                                                    <Templates templates={customer.templates}/>
-                                                ) : ("")}
-                                                <button className="btn">SIGN OUT</button>
-                                            </div>
+                                                    {customer.templates ? (
+                                                        <Templates templates={customer.templates}/>
+                                                    ) : ("")}
+                                                    <button className="btn">SIGN OUT</button>
+                                                </div>
 
-                                        ) : ("")}
+                                            ) : ("")}
+                                        </div>
+
                                     </>
                                 ) : (
                                     <a href="#"><Icon icon={farUserCircle} className="pr-1.5"/>{customer.title}<Icon
@@ -168,12 +188,13 @@ export default function MiddleBar({data, customer}) {
 
 
                         </li>
-                        <li className="py-30px -my-30px">
+                        <li className="md:py-30px md:-my-30px">
                             <a href="#"><Icon icon={faShoppingBag} className="pr-1.5"/>5</a>
                         </li>
                     </ul>
                 </div>
-                <InstantSearch data={[]} instantSearchState={instantSearchState} setInstantSearchState={setInstantSearchState}/>
+                <InstantSearch data={[]} instantSearchState={instantSearchState}
+                               setInstantSearchState={setInstantSearchState}/>
             </Container>
 
         </div>
--- components/header/search/instant-search.js
+++ components/header/search/instant-search.js
@@ -1,8 +1,11 @@
 import React, {useState} from "react";
 import Link from "next/link";
 import Icon from "@components/icon";
+import {useCurrentWidth} from "../../../lib/helpers";
 
 export default function InstantSearch({data, instantSearchState, setInstantSearchState}) {
+    let width = useCurrentWidth();
+
     const onSearchResultMouseEnter = () => {
         setInstantSearchState(prevState => {
             return {...prevState, mouseOn: true}
@@ -14,39 +17,67 @@ export default function InstantSearch({data, instantSearchState, setInstantSearc
         });
     }
 
+    let isMobile = (width<=768)
+    let checkIsActive =((instantSearchState.value && instantSearchState.value.length > 0) || isMobile)
+
+
     return (
 
-        (instantSearchState.value && instantSearchState.value.length > 0) ? (
-            <div
-                onMouseEnter={onSearchResultMouseEnter}
-                onMouseLeave={onSearchResultMouseLeave}
-                className="absolute left-1/2 w-5/12 transform -translate-x-1/3 top-full bg-white border border-gray_border border-solid">
-                <div className="flex">
-                    <div className="w-2/3 min-h-instant p-7">
-                        <ul className="flex">
-                            <li className="font-extrabold text-sm text-gray_4">Top Results for
-                                “{instantSearchState.value}”
-                            </li>
-                            <li className="ml-auto">
-                                <Link href="#">
-                                    <a>View All Results</a>
-                                </Link>
-                            </li>
-                        </ul>
+        <div
+            onMouseEnter={onSearchResultMouseEnter}
+            onMouseLeave={onSearchResultMouseLeave}
+            className={"block md:absolute md:left-1/2  md:w-5/12 md:transform md:-translate-x-1/3 top-full bg-white border border-gray_border border-solid "+((checkIsActive || isMobile)?"block":"hidden")}>
+            <div className="block md:flex">
+                <input
+                    className="pl-5 text-sm text-gray_4 focus:outline-none w-full block md:hidden"
+                    placeholder="Search"
+                />
+                <div className="w-full md:w-2/3 min-h-instant p-7">
+                    <ul className="hidden md:flex">
+                        <li className="font-extrabold text-sm text-gray_4">Top Results for
+                            “{instantSearchState.value}”
+                        </li>
+                        <li className="ml-auto">
+                            <Link href="#">
+                                <a>View All Results</a>
+                            </Link>
+                        </li>
+                    </ul>
 
+                    {
+
+                        instantSearchState.searchResult ? (
+                            <ul className="block md:flex md:flex-wrap pt-4 md:-mx-6">
+                                {
+                                    instantSearchState.searchResult.data.result.map((link) => (
+                                        <li className="w-full md:w-1/3 px-6 pb-2.5 md:pb-7 md:flex-grow" key={"search-result-" + link.id}>
+
+                                            <Link href={link.url}>
+                                                <a>
+                                                    <img className="hidden md:block pb-2.5" src={link.image}/>
+                                                    <h4 className="text-xs">{link.text}</h4>
+                                                </a>
+                                            </Link>
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        ) : ("")
+                    }
+
+                </div>
+                <div className="w-1/3 hidden md:block bg-gray_1 py-7 px-11">
+                    <h4 className="font-extrabold text-sm text-gray_4 pb-4">Suggestions</h4>
+                    <div>
                         {
-
-                          instantSearchState.searchResult ? (
-                                <ul className="flex flex-wrap pt-4 -mx-6">
+                            instantSearchState.searchResult ? (
+                                <ul>
                                     {
-                                        instantSearchState.searchResult.data.result.map((link) => (
-                                            <li className="w-1/3 px-6 pb-7 flex-grow" key={"search-result-" + link.id}>
+                                        instantSearchState.searchResult.data.suggestions.map((link) => (
+                                            <li className="pb-2.5 text-gray_2" key={"search-suggestion-" + link.id}>
 
                                                 <Link href={link.url}>
-                                                    <a>
-                                                        <img className="pb-2.5" src={link.image}/>
-                                                        <h4 className="text-xs">{link.text}</h4>
-                                                    </a>
+                                                    <a>{link.text}</a>
                                                 </Link>
                                             </li>
                                         ))
@@ -54,50 +85,28 @@ export default function InstantSearch({data, instantSearchState, setInstantSearc
                                 </ul>
                             ) : ("")
                         }
-
                     </div>
-                    <div className="w-1/3 bg-gray_1 py-7 px-11">
-                        <h4 className="font-extrabold text-sm text-gray_4 pb-4">Suggestions</h4>
-                        <div>
-                            {
-                                instantSearchState.searchResult ? (
-                                    <ul>
-                                        {
-                                            instantSearchState.searchResult.data.suggestions.map((link) => (
-                                                <li className="pb-2.5 text-gray_2" key={"search-suggestion-" + link.id}>
+                    <h4 className="font-extrabold text-sm text-gray_4 pb-4 pt-4">Categories</h4>
+                    <div>
+                        {
+                            instantSearchState.searchResult ? (
+                                <ul>
+                                    {
+                                        instantSearchState.searchResult.data.categories.map((link) => (
+                                            <li className="pb-2.5 text-gray_2" key={"search-category-" + link.id}>
 
-                                                    <Link href={link.url}>
-                                                        <a>{link.text}</a>
-                                                    </Link>
-                                                </li>
-                                            ))
-                                        }
-                                    </ul>
-                                ) : ("")
-                            }
-                        </div>
-                        <h4 className="font-extrabold text-sm text-gray_4 pb-4 pt-4">Categories</h4>
-                        <div>
-                            {
-                                instantSearchState.searchResult ? (
-                                    <ul>
-                                        {
-                                            instantSearchState.searchResult.data.categories.map((link) => (
-                                                <li className="pb-2.5 text-gray_2" key={"search-category-" + link.id}>
-
-                                                    <Link href={link.url}>
-                                                        <a>{link.text}</a>
-                                                    </Link>
-                                                </li>
-                                            ))
-                                        }
-                                    </ul>
-                                ) : ("")
-                            }
-                        </div>
+                                                <Link href={link.url}>
+                                                    <a>{link.text}</a>
+                                                </Link>
+                                            </li>
+                                        ))
+                                    }
+                                </ul>
+                            ) : ("")
+                        }
                     </div>
                 </div>
             </div>
-        ) : ("")
+        </div>
     )
 }
